ecc90d389ccfe2653fea9dbd023dd2093d326e2c
kanjamad bosel <kanjamadpor@gmail.com>
2019-05-14 20:47:38 -0700

    first commit

--- bank.py
+++ bank.py
@@ -2,14 +2,14 @@ name = input('What is your name? ')
 print('Hello *(' + name + ')* welcome to BankAccount')
 
 class BankAccount():
-    def __init__(self, account_type, name=''):
+    def __init__(self, account_type, balance ,name):
         self.name = name
         self.account_type = account_type
-        self.balance = 0
+        self.balance = balance
         # print(name, "create =>", self)
     
     def __str__(self):
-        return 'BankAccount -> type: {}, balance: {}'.format(self.account_type, self.balance)
+        return 'BankAccount -> type: {}, balance: {}, name: {}'.format(self.account_type, self.balance, self.name)
 
     def bank_account(self):
         # print("Hello!!!! I have", self.savings,  "and In have" ,self.checking, )
@@ -30,7 +30,12 @@ class BankAccount():
 # print(kanjamad_account.bank_account())
 
 
-kanjamad_savings = BankAccount('Savings', 'Kanjamad')
-kanjamad_checking = BankAccount('Checking', 'Kanjamad')
+kanjamad_savings = BankAccount('Savings', '$300,000','Kanjamad')
+kanjamad_checking = BankAccount('Checking','$600,000' ,'Kanjamad')
 print(kanjamad_savings)
 print(kanjamad_checking)
+
+Alex_savings = BankAccount('Savings', '$130,000','Alex')
+Alex_checking = BankAccount('Checking','$690,000' ,'Alex')
+print(Alex_savings)
+print(Alex_checking)
